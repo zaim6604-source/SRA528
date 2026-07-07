a9acb1aa0bc4c344af0339e054544ce3f3f73f96
Rana Adnan Gul <rana.gul755@gmail.com>
2026-07-07 12:45:08 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Countries from './components/Countries';
 import Testimonials from './components/Testimonials';
-import CTABand from './components/CTABand';
+import CTABand from './components/CtaBand';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
